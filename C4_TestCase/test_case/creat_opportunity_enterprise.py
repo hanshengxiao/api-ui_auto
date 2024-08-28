55628f0ef521d1f01681883cdb35a447531a730b
Victor.han <102147435+hanshengxiao@users.noreply.github.com>
2024-08-28 17:59:27 +0800

    123

---
+++ C4_TestCase/test_case/creat_opportunity_enterprise.py
@@ -0,0 +1,238 @@
+import datetime
+import logging
+import time
+
+from C4_TestCase.test_base.locators import LocatorActions
+from selenium import webdriver
+from selenium.common import TimeoutException, NoSuchElementException, WebDriverException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+# 获取当前时间
+now = datetime.datetime.now()
+month_day_time = now.strftime("%m%d-%H%M")
+
+# 配置日志记录
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# Chrome 浏览器配置s
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument('--disable-cache')
+def initialize_driver():
+    try:
+        # WebDriver 配置
+        webdriver_service = Service(r'E:\Webdriver\chromedriver-win64\chromedriver-win64\chromedriver.exe')
+        driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+        logging.info("Chrome 浏览器初始化成功")
+        return driver
+    except WebDriverException as e:
+        logging.error(f"Chrome 浏览器初始化失败: {e}")
+        raise
+
+# 登录函数
+def login(driver):
+    try:
+        driver.get(
+            'https://cas-server.glodon.com/cas/login?service=https%3A%2F%2Fcas-server.glodon.com%2Fcas%2Fidp%2Fprofile%2FSAML2%2FCallback%3FentityId%3DHTTPS%253A%252F%252Fmy500749-sso.c4c.saphybriscloud.cn')
+        time.sleep(2)
+        driver.find_element(By.ID, 'username').send_keys('LTC-3')
+        driver.find_element(By.ID, 'password').send_keys('Glodon@2023')
+        driver.find_element(By.ID, 'SM_BTN_1').click()
+        time.sleep(3)
+        logging.info("登录成功")
+    except NoSuchElementException as e:
+        logging.error(f"登录元素未找到: {e}")
+        driver.quit()
+    except Exception as e:
+        logging.error(f"登录过程中出现异常: {e}")
+        driver.quit()
+
+# 测试用例1：打开页面并最大化窗口
+def test_case1(driver):
+    try:
+        driver.get(
+            'https://my500749-sso.c4c.saphybriscloud.cn/sap/ap/ui/repository/SAP_UI/HTML5/newclient.html?app.component=/SAP_UI_CT/Main/root.uiccwoc&rootWindow=X&redirectUrl=/sap/byd/runtime#Nav/0/eyJfc1dvY0lkIjpudWxsLCJfc1ZpZXdJZCI6bnVsbCwiX3NQZXJmb3JtYW5jZV9zUGVyZm9ybWFuY2VWaWV3SWQiOiJIb21lIiwiX3NDb250ZXh0SWQiOiIyMzJkMTY2Mjk3YTk5MTRjN2UxNGQ0YzA4YTNhODA2MiJ9')
+        driver.maximize_window()
+        logging.info("页面打开并最大化")
+    except Exception as e:
+        logging.error(f"测试用例1执行过程中出现异常: {e}")
+
+
+# 测试用例2：执行一系列操作
+def test_case2(driver, locator):
+    try:
+        locator.wait_and_click("//span[@title='销售']", 100)
+        locator.wait_and_click("//a[@title='商机']", 30)
+
+        # 尝试关闭超期提醒
+        try:
+            overdue_confirm_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//button[.//bdi[text()="确认"]]'))
+            )
+            overdue_confirm_button.click()
+            logging.info("超期提醒已关闭")
+        except TimeoutException:
+            logging.info("未检测到超期提醒，继续执行下一步")
+
+        locator.wait_and_click("//span[contains(@data-sap-ui, 'xf')]")
+        locator.wait_and_click("//span[@data-sap-automation-id='multiTab-FirstTab-Text2']")
+        locator.wait_and_click("//span[@data-sap-automation-id='multiTab-Text2-新建商机']", 100)
+        time.sleep(5)
+
+        # 输入商机名称
+        logging.info("输入商机名称")
+        locator.wait_and_send_keys("//input[contains(@maxlength, '255')]", f"自动化测试商机{month_day_time}")
+
+        # 选择客户
+        logging.info("选择客户")
+        time.sleep(3)
+        locator.wait_and_send_keys('//*[@id="objectvalueselectorHppqevK8aaYFSgcH3xeA1G_348-inputField-inner"]',
+                                   "平安喜乐 1000043004")
+        driver.find_element(By.XPATH,
+                            '//*[@id="objectvalueselectorHppqevK8aaYFSgcH3xeA1G_348-inputField-inner"]').send_keys(
+            Keys.ENTER)
+
+        # 点击并选择商机类型
+        logging.info("选择商机类型")
+        time.sleep(3)
+        locator.wait_and_click('//*[@id="dropdownlistbox6db9c5750fd2a06b9d82f9cabfc49b74_352-inner"]')
+        time.sleep(3)
+        locator.wait_and_click("//li[contains(@class, 'sapMLIB') and .//div[contains(text(), '企业级')]]")
+
+        # 选择联系人
+        logging.info("选择联系人")
+        time.sleep(2)
+        locator.wait_and_send_keys('//*[@id="objectvalueselectorDJFL3kMLhK_OJoDgE62YbW_360-inputField-inner"]',
+                                   "宋秀津")
+        driver.find_element(By.XPATH,
+                            '//*[@id="objectvalueselectorDJFL3kMLhK_OJoDgE62YbW_360-inputField-inner"]').send_keys(
+            Keys.ENTER)
+
+        # 选择联系人角色
+        logging.info("选择联系人角色")
+        time.sleep(3)
+        locator.wait_and_send_keys_by_id('dropdownlistbox7c095e0122254f5bbe23443c743896d6_384-inner', "使用人")
+        locator.wait_and_click_by_id('__item230-content', 30)
+
+        # 点击并选择来源
+        logging.info("选择来源")
+        time.sleep(3)
+        locator.wait_and_click_by_id('dropdownlistboxmyokgu9MOqQ2YL10sdWYN0_364-arrow', 30)
+        time.sleep(3)
+        locator.wait_and_click("//li[contains(@class, 'sapMLIB') and .//div[contains(text(), '客户拜访')]]")
+
+        # 点击并选择对计划的态度
+        logging.info("选择对计划的态度")
+        time.sleep(3)
+        locator.wait_and_click_by_id('dropdownlistboxbf8d813660b080d12db0ee642ab52970_388-arrow', 30)
+        time.sleep(3)
+        locator.wait_and_click("//li[contains(@class, 'sapMLIB') and .//div[contains(text(), '支持')]]")
+
+        # 点击并选择实际购买层级
+        logging.info("选择实际购买层级")
+        time.sleep(3)
+        locator.wait_and_click_by_id('dropdownlistbox1824dff254b1e84c03f67f7a307bdd6c_408-arrow', 30)
+        time.sleep(3)
+        locator.wait_and_click("//li[contains(@class, 'sapMLIB') and .//div[contains(text(), '集团公司/局公司')]]")
+
+        # 点击并选择是否有规划
+        logging.info("选择是否有规划")
+        time.sleep(3)
+        locator.wait_and_click_by_id('dropdownlistbox7d9a4f7280a67b7d935aa644bfa342bd_623-arrow', 30)
+        time.sleep(3)
+        locator.wait_and_click("//li[contains(@class, 'sapMLIB') and .//div[contains(text(), '是')]]")
+
+        # 点击并选择是否有负责人
+        logging.info("选择是否有负责人")
+        time.sleep(3)
+        locator.wait_and_send_keys_by_id('dropdownlistboxc48f243ef19ff574ce8ea8648732b880_627-inner', "是")
+
+
+        # 点击并选择是否有需求
+        logging.info("选择是否有需求")
+        time.sleep(3)
+        locator.wait_and_send_keys_by_id('dropdownlistboxfa17bfb6e2a94344dbe4dbbb8f889df4_631-inner', "是")
+
+
+        # 点击并选择是否有预算
+        logging.info("选择是否有预算")
+        time.sleep(3)
+        locator.wait_and_send_keys_by_id('dropdownlistbox4ea225a267c5a45982364c556b6d9072_635-inner', "是")
+        time.sleep(3)
+        driver.find_element(By.ID,
+                            'dropdownlistbox4ea225a267c5a45982364c556b6d9072_635-inner').send_keys(
+            Keys.ENTER)
+
+
+        # 添加产品--企业级
+        logging.info("添加产品--企业级")
+        time.sleep(3)
+        locator.wait_and_click_by_id('button3vks4Vj2c4Y2G2O3ss9AM0_962-inner', 15)
+        time.sleep(4)
+        locator.wait_and_click("//span[contains(@data-sap-ui, 'objectvalueselectorh')]", 15)
+        time.sleep(3)
+        locator.wait_and_click("//span[@title='其他']", 15)
+        time.sleep(1)
+        locator.wait_and_send_keys("//input[@value='0.00']", "43000")
+
+        # 保存企业级商机
+
+        time.sleep(15)
+        logging.info("保存企业级商机")
+        locator.wait_and_click("//bdi[contains(@id, 'aac')]", 15)
+
+        print(month_day_time)
+
+
+
+    except TimeoutException as e:
+        logging.error(f"元素定位超时: {e}")
+        logging.debug(driver.page_source)
+    except Exception as e:
+        logging.error(f"测试用例2执行过程中出现异常: {e}")
+
+        logging.debug(driver.page_source)
+    finally:
+        logging.info("测试用例2执行完毕")
+
+# 主函数
+def main():
+    try:
+        # 初始化 WebDriver
+        driver = initialize_driver()
+
+        # 初始化定位方法类
+        locator = LocatorActions(driver)
+
+        # 执行登录操作
+        login(driver)
+
+        # 执行测试用例1
+        test_case1(driver)
+
+        # 执行测试用例2
+        test_case2(driver, locator)
+        print("自动化测试商机"+month_day_time)
+
+    except Exception as e:
+        logging.error(f"脚本执行过程中出现异常: {e}")
+    finally:
+        # 设置日志记录器
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        # 初始化倒计时的秒数
+        seconds_left = 20
+        # 开始倒计时
+        while seconds_left > 0:
+            logging.info(f"剩余时间: {seconds_left}秒")
+            time.sleep(1)  # 暂停1秒
+            seconds_left -= 1
+        logging.info("倒计时结束")
+        # 关闭浏览器
+        driver.quit()
+if __name__ == "__main__":
+    main()
---
+++ C4_TestCase/test_case/test_creat_contract_Independent.py
@@ -0,0 +1,285 @@
+# -*- coding: utf-8 -*-
+import json
+import logging
+import sys
+import time
+
+import mysql.connector
+import requests
+from mysql.connector import Error
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class TestAutomation:
+    def __init__(self):
+        self.order_id = None
+        self.opportunity_name = "0828-1721"
+        self.cart_number = 2
+        self.opportunity_id = None
+        self.driver = None
+        self.chrome_options = Options()
+        self.chrome_options.add_argument("--start-maximized")
+        self.chrome_options.add_argument("--disable-gpu")
+        self.chrome_options.add_argument('--disable-cache')
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    def connect_to_database(self):
+        try:
+            conn = mysql.connector.connect(
+                host='10.0.106.61',
+                port=8010,
+                user='opportunity',
+                password='opportunitytest2024',
+                database='opportunity'
+            )
+            if conn.is_connected():
+                cursor = conn.cursor()
+                query = "SELECT opportunity_num FROM opportunity.g_opportunity WHERE opportunity_name LIKE %s"
+                cursor.execute(query, ('%' + self.opportunity_name + '%',))
+                result = cursor.fetchone()
+                self.opportunity_id = result[0] if result else None
+                if self.opportunity_id:
+                    logging.info(f"检索到的 opportunity_id: {self.opportunity_id}")
+                else:
+                    logging.error("未找到与给定 opportunity_name 匹配的 opportunity_id")
+        except Error as e:
+            logging.error(f"数据库连接错误: {e}")
+        finally:
+            if conn.is_connected():
+                cursor.close()
+                conn.close()
+
+        if not self.opportunity_id:
+            logging.error("opportunity_id 为空，停止执行")
+            sys.exit()
+    def initialize_driver(self):
+        try:
+            webdriver_service = Service(r'E:\Webdriver\chromedriver-win64\chromedriver-win64\chromedriver.exe')
+            self.driver = webdriver.Chrome(service=webdriver_service, options=self.chrome_options)
+            logging.info("Chrome 浏览器初始化成功")
+        except WebDriverException as e:
+            logging.error(f"Chrome 浏览器初始化失败: {e}")
+            raise
+    def close_driver(self):
+        if self.driver:
+            self.driver.quit()
+            logging.info("浏览器已关闭")
+    def login(self, max_retries=1):
+        retry_count = 0
+        while retry_count < max_retries:
+            try:
+                self.driver.get('https://cas-server.glodon.com/cas/login?service=http://zjtest.gyuncai.com/login/sso/glodonLogin')
+                self.driver.find_element(By.ID, 'username').send_keys('LTC-3')
+                self.driver.find_element(By.ID, 'password').send_keys('Glodon@2023')
+                time.sleep(2)
+                self.driver.find_element(By.ID, 'SM_BTN_1').click()
+                logging.info("登录成功")
+                return True
+            except NoSuchElementException as e:
+                logging.error(f"登录元素未找到 (重试 {retry_count + 1}/{max_retries}): {e}")
+            except Exception as e:
+                logging.error(f"登录过程中出现异常 (重试 {retry_count + 1}/{max_retries}): {e}")
+
+            retry_count += 1
+            logging.info(f"正在重试登录... ({retry_count}/{max_retries})")
+            time.sleep(3)
+
+        logging.error("登录失败，已达到最大重试次数")
+        self.close_driver()
+        return False
+    def execute_test_cases(self):
+        try:
+            try:# self.driver.get("https://zjtest.gyuncai.com/mall/mall-view-admin/mallQuotation")
+                self.driver.maximize_window()
+                logging.info("页面打开并最大化")
+                time.sleep(5)
+            except Exception as e:
+               logging.error(f"测试用例1执行过程中出现异常: {e}")
+
+            # 从Selenium中获取cookies
+            logging.info("正在获取cookies...")
+            time.sleep(2)
+            selenium_cookies = self.driver.get_cookies()
+            session = requests.Session()
+            for cookie in selenium_cookies:
+                session.cookies.set(cookie['name'], cookie['value'])
+
+            url = "https://zjtest.gyuncai.com/material/mallQuotation/insertQuotationFromShopCart"
+            headers = {
+                "Content-Type": "application/json"
+            }
+            data =  {
+                "contractInfo": "独立合同",
+                "opportunityId": self.opportunity_id,
+                "opportunityName": "自动化测试商机-"+self.opportunity_name,
+                "accountContactName": "宋秀津",
+                "cartNum": [
+                    #16002732-广联达斑马进度技术服务费
+                    {"cartId": 75948,"cartNum": self.cart_number,"packageList": []},
+                    #10000477-12V电源
+                    {"cartId": 75947,"cartNum": self.cart_number,"packageList": []},
+                    #10001866-花生壳（物料验收）
+                    {"cartId": 75946,"cartNum": self.cart_number,"packageList": []},
+                    #15000078-广联达梦龙物料现场验收管控系统 V3.8
+                    {"cartId": 75945,"cartNum": self.cart_number,"packageList": []},
+                    #13000682-GEPS云服务费-20点及以下
+                    {"cartId": 75944,"cartNum": self.cart_number,"packageList": []},
+                    #15000145-广联达梦龙施工企业项目管理V9.5（GEPS）-项目信息管理
+                    {"cartId": 75943,"cartNum": self.cart_number,"packageList": []},
+                    # 16003999-广联达斑马进度计划软件-编辑版-年费制
+                    {"cartId": 75942,"cartNum": self.cart_number,"packageList": []}
+                ],
+                "sourceType": 4,
+                "saleOrgName": "广联达科技股份有限公司",
+                "quotationName": "自动化测试合同-"+self.opportunity_name,
+                "c4ClientCode": "1000043004",
+                "accountID": "1000043004",
+                "accountName": "平安喜乐",
+                "sellerEmployeeId": "60000003",
+                "sellerEmployeeName": None,
+                "sellerEmployeeMobile": None
+            }
+            json_data = json.dumps(data, ensure_ascii=False, indent=4)
+            print(json_data)
+            response = session.post(url, json=data, headers=headers)
+
+            if response.status_code == 200:
+                response_data = response.json()
+                order_id = response_data.get('data')
+                if order_id:
+                    logging.info(f"POST请求成功: {response_data}")
+                    self.driver.get(
+                        f"https://zjtest.gyuncai.com/mall/mall-view-admin/quotationDetail/?orderId={order_id}")
+                    self.order_id = order_id
+                else:
+                    logging.error(f"POST请求成功，但未返回 order_id: {response_data}")
+            else:
+                logging.error(f"POST请求失败，状态码: {response.status_code}，响应: {response.text}")
+
+            # File upload
+            url_upload = "https://zjtest.gyuncai.com/base/upload/uploadFileToObs?type=estimate"
+            files = [
+                ('file', (
+                    '项目概算利润表导入模板 (2).xlsx',
+                    open(r'C:\Users\tiezh\Downloads\项目概算利润表导入模板 (2).xlsx', 'rb'),
+                    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'))
+            ]
+            headers_upload = {
+                'accept': 'application/json, text/plain, */*',
+                'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+                'origin': 'https://zjtest.gyuncai.com',
+                'personalflag': 'enterprise',
+                'priority': 'u=1, i',
+                'referer': f'https://zjtest.gyuncai.com/mall/mall-view-admin/quotationDetail/?orderId={self.order_id}',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0',
+                'x-requested-with': 'XMLHttpRequest',
+                'x-top-window': 'true'
+            }
+            response_upload = session.post(url_upload, headers=headers_upload, files=files)
+            logging.info(f"文件上传响应: {response_upload.text}")
+
+            # Estimate upload
+            url_estimate = "https://zjtest.gyuncai.com/api/material/customer/estimate/uploadEstimate"
+            payload_estimate = {
+                "quotationId": str(self.order_id),
+                "fileUrl": "test/estimate/f1343b1244dd46bc8624e3b236f1e048.xlsx",
+                "fileName": "项目概算利润表导入模板 (2).xlsx",
+                "jobNumber": ""
+            }
+            headers_estimate = {
+                'accept': 'application/json, text/plain, */*',
+                'content-type': 'application/json;charset=UTF-8',
+                'origin': 'https://zjtest.gyuncai.com',
+                'personalflag': 'enterprise',
+                'priority': 'u=1, i',
+                'referer': f'https://zjtest.gyuncai.com/mall/mall-view-admin/quotationDetail/?orderId={self.order_id}',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0',
+                'x-requested-with': 'XMLHttpRequest',
+                'x-top-window': 'true'
+            }
+            response_estimate = session.post(url_estimate, headers=headers_estimate, json=payload_estimate)
+            logging.info(f"上传估算响应: {response_estimate.text}")
+
+        except TimeoutException as e:
+            logging.error(f"元素定位超时: {e}")
+            logging.debug(self.driver.page_source)
+        except Exception as e:
+            logging.error(f"测试用例执行过程中出现异常: {e}")
+            logging.debug(self.driver.page_source)
+        finally:
+            logging.info("测试用例执行完毕")
+    def test_case2(self):
+        time.sleep(5)
+        logging.info("测试用例2开始执行")
+        self.driver.find_element(By.XPATH,'//*[@id="app-main"]/div/div[2]/div[1]/div[1]/form/div[6]/div/label/span[1]/span').click()
+        time.sleep(3)
+        logging.info("测试用例2开始执行提交")
+        self.driver.find_element(By.XPATH,'//*[@id="app-main"]/div/div[2]/div[1]/div[2]/div/button[1]/span').click()
+        time.sleep(3)
+        self.driver.find_element(By.XPATH,'//*[@id ="app-main"]/div/div[2]/div[1]/div[3]/div[3]/div/div[3]/span/button[2]').click()
+        # 尝试关闭超期提醒
+        time.sleep(3)
+        try:
+            self.driver.find_element(By.XPATH,'/html/body/div[5]/div/div[3]/button[2]/span').click()
+            logging.info("报价单包含非固定成本价物料，请您注意核对物料成本价提醒已关闭")
+        except TimeoutException:
+            logging.info("未检测到提醒，继续执行下一步")
+
+
+
+        time.sleep(10)
+        self.driver.refresh()
+        time.sleep(5)
+        self.driver.refresh()
+        time.sleep(5)
+        self.driver.refresh()
+        time.sleep(5)
+        self.driver.refresh()
+        time.sleep(5)
+        self.driver.refresh()
+        time.sleep(5)
+        self.driver.refresh()
+
+        # 等待元素出现
+        wait = WebDriverWait(self.driver, 1000)
+        element = wait.until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="pane-productList"]/div[1]/div/div[1]/div/span/p[1]')))
+        # 断言元素文本
+        assert element.text == "合同已生成", f"期望的文本是 '合同已生成'，但实际找到的是 '{element.text}'"
+
+        logging.info("测试用例2执行完毕")
+
+
+        # 设置日志记录器
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        # 初始化倒计时的秒数
+        seconds_left = 30
+        # 开始倒计时
+        while seconds_left > 0:
+            logging.info(f"剩余时间: {seconds_left}秒")
+            time.sleep(1)  # 暂停1秒
+            seconds_left -= 1
+        logging.info("倒计时结束")
+        # 关闭浏览器
+        self.driver.quit()
+    def run(self):
+        try:
+            self.connect_to_database()
+            self.initialize_driver()
+            if self.login():
+                self.execute_test_cases()
+                self.test_case2()
+        except Exception as e:
+            logging.error(f"脚本执行过程中出现异常: {e}")
+        finally:
+            self.close_driver()
+
+if __name__ == "__main__":
+    automation = TestAutomation()
+    automation.run()
+# -*- coding: utf-8 -*-
